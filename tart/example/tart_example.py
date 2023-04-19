3722dfc3bc4b8bbc76baabf69c2fd6f215f9d0f3
manishshettym <manishs@berkeley.edu>
2023-04-18 18:13:13 -0700

    added support for default encoders: CodeBERT, and CodeBERT-BPE

--- tart/example/tart_example.py
+++ tart/example/tart_example.py
@@ -1,31 +1,8 @@
 import torch
-from transformers import RobertaTokenizer, RobertaModel
 
+from tart.representation.encoders import get_feature_encoder
 from tart.representation.train import tart_train
 from tart.representation.test import tart_test
-
-my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-codebert_name = "microsoft/codebert-base"
-CodeBertTokenizer = RobertaTokenizer.from_pretrained(codebert_name)
-CodeBertModel = RobertaModel.from_pretrained(codebert_name).to(my_device)
-CodeBertModel.eval()
-
-
-def feat_encoder(x: str) -> torch.tensor:
-    tokens_ids = CodeBertTokenizer.encode(
-        x, truncation=True)
-    
-    tokens_tensor = torch.tensor(tokens_ids, device=my_device)
-    
-    with torch.no_grad():
-        context_embeddings = CodeBertModel(
-            tokens_tensor[None, :])[0]
-    
-    encoding = torch.mean(context_embeddings, dim=1)
-    
-    return encoding
-
 
 if __name__ == "__main__":
     torch.multiprocessing.set_start_method('spawn')
@@ -33,13 +10,11 @@ if __name__ == "__main__":
     # config file path
     config_file = "tart-config.json"
 
+    # feature encoder
+    feat_encoder = get_feature_encoder("CodeBert")
+
     # call train API
     tart_train(config_file, feat_encoder)
 
     # call test API
     tart_test(config_file, feat_encoder)
-
-
-# TODO: Give 2 predefined for text encoders
-# BPE and CodeBERT
-# custom is also possible (as shown above)
