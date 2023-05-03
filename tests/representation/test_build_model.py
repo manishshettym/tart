a849f28cbdfb909a4be7831e616c3314e5c33112
manishshettym <manishs@berkeley.edu>
2023-05-02 17:15:03 -0700

    making setup CPU by default; gpu option added

--- tests/representation/test_build_model.py
+++ tests/representation/test_build_model.py
@@ -5,12 +5,13 @@ from tart.representation import models
 from tart.utils.model_utils import build_model
 import tart.representation.config as config
 
+
 def setup_test_args(user_config_file="./tests/data/sample_config.json"):
     parser = argparse.ArgumentParser()
-    
+
     # add dummy argument for pytest
     parser.add_argument("-s", dest="s", type=str, help="pytest folder")
-    
+
     # reading user config from json file
     with open(user_config_file) as f:
         config_json = json.load(f)
@@ -21,15 +22,16 @@ def setup_test_args(user_config_file="./tests/data/sample_config.json"):
     config.build_feature_configs(parser)
 
     args = parser.parse_args()
-    
+
     # set user defined configs
     args = config.init_user_configs(args, config_json)
-    
+
     return args
+
 
 def test_model_build():
     args = setup_test_args()
     model = build_model(models.SubgraphEmbedder, args)
     model_dict = model.state_dict()
-    
+
     assert len(model_dict.keys()) == 62
--- tests/utils/test_feat_graph.py
+++ tests/utils/test_feat_graph.py
@@ -5,12 +5,13 @@ from tart.utils.graph_utils import read_graph_from_json, featurize_graph
 from tart.representation.encoders import get_feature_encoder
 import tart.representation.config as config
 
+
 def setup_test_args(user_config_file="./tests/data/sample_config.json"):
     parser = argparse.ArgumentParser()
-    
+
     # add dummy argument for pytest
     parser.add_argument("-s", dest="s", type=str, help="pytest folder")
-    
+
     # reading user config from json file
     with open(user_config_file) as f:
         config_json = json.load(f)
@@ -21,23 +22,22 @@ def setup_test_args(user_config_file="./tests/data/sample_config.json"):
     config.build_feature_configs(parser)
 
     args = parser.parse_args()
-    
+
     # set user defined configs
     args = config.init_user_configs(args, config_json)
-    
+
     return args
 
 
 def test_read_graph():
     args = setup_test_args()
     sample_graph_json = "./tests/data/sample_graph.json"
-    
+
     graph = read_graph_from_json(args, sample_graph_json)
     feat_encoder = get_feature_encoder(args.feat_encoder)
-    
+
     assert feat_encoder is not None
-    
+
     dsgraph = featurize_graph(args, feat_encoder, graph, anchor=None)
     assert dsgraph.num_nodes == 3
     assert dsgraph.num_edges == 3
-    
