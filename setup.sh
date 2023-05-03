ce151529c23f21d5053c88fafbe0b86bf8b6aa89
manishshettym <manishs@berkeley.edu>
2023-05-02 17:23:40 -0700

    add torch to setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # setup script for installing deepsnap and torch_geometric
 
-# GPU (assumes Cuda 11.6)
+# GPU (assumes Cuda 11.6, Python 3.9, and PyTorch 1.13.0)
 # https://download.pytorch.org/whl/torch_stable.html
 # https://pypi.org/project/torch-scatter/
 
@@ -13,6 +13,7 @@
 # CPU
 # https://stackoverflow.com/questions/65860764/pytorch-torch-sparse-installation-without-cuda
 
+pip install https://download.pytorch.org/whl/cpu/torch-1.13.0%2Bcpu-cp310-cp310-linux_x86_64.whl
 pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-1.13.0+cpu.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-1.13.0+cpu.html
 pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-1.13.0+cpu.html
